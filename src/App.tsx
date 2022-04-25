83eeecbc08ff8077411068107e19f072887d8fd6
alexsandersilv <alexssilva.contato@gmail.com>
2022-04-25 07:45:10 -0300

    Aula 01

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { ExperienceBar } from './components/ExperienceBar';
+
+function App() {
+    return (
+        <div className="container">
+            <ExperienceBar />
+        </div>
+    );
+}
+
+export default App
